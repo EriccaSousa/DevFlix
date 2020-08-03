fba86e82b52ef2d6e391946e85e3d94fa528b450
Erica Sousa <ericams175@gmail.com>
2020-08-03 14:09:50 -0300

    add alterações no readme, e personalizando form.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # DevFlix
+
+<p align="center">
+  <img alt="Logo do projeto" width="150px" src="./src/assets/img/logo.png" />
+  <img alt="Logo do projeto" width="150px" src="https://www.alura.com.br/assets/img/imersoes/react/imersao-react-logo.1594044142.svg" />
+</p>
+
+> Projeto construido durante a [Imersão React da Alura](https://www.alura.com.br/imersao-react/)
+
+
+## Como rodar o projeto?
+
+Após baixar o projeto, acesse ele via seu terminal rode os comandos:
+
+```sh
+npm install
+npm start
+```
--- src/components/Pages/Cadastro/Categoria/index.js
+++ src/components/Pages/Cadastro/Categoria/index.js
@@ -1,27 +1,91 @@
-import React from 'react';
-import PageDefault from '../../../PagesDefault';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../PagesDefault';
+import FormField from '../../../FormField';
 
 function CadastroCategoria() {
-    return(
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  }
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
-            <form>
 
-                <label>
-                    Nome da Categoria:
-                    <input type="text"/>
-                </label>
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    })
+  }
 
-                <button>Cadastrar</button>
-            </form>
-
-            <Link to="/">
-                Ir para Home
-            </Link>
-        </PageDefault>
+  function handleChange(infosDoEvento) {
+    setValue(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value
     );
+  }
+
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+          setCategorias([
+            ...categorias,
+            values
+          ]);
+
+          setValues(valoresIniciais)
+      }}>
+
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição:"
+          type="????"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+      
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        })}
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
 export default CadastroCategoria;
